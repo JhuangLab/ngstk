e99d608e95f792ec8f57b80f472d26a259f7f4ec
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2017-09-29 20:05:42 +0800

    fusion2pp and fusion2pp_meta be added

--- R/proteinpaint.R
+++ R/proteinpaint.R
@@ -2,6 +2,8 @@
 #'
 #' @param input_data A mutation data.frame need to be converted to ProteinPaint input.
 #' @param input_type Point the input data format (iseq or others)
+#' @param config_file ngstk ProteinPaint configuration file path, default is 
+#' system.file("extdata", "config/proteinpaint.toml", package = "ngstk")
 #' 
 #' @return
 #' A data frame
@@ -14,82 +16,214 @@
 #' input_data$disease <- as.character(input_data$disease)
 #' mut2pp(input_data, input_type = "iseq")
 
-mut2pp <- function(input_data, input_type = "iseq") {
-  config_file <- system.file("extdata", "config/proteinpaint.toml", package = "ngstk")
+mut2pp <- function(input_data, input_type = "iseq", 
+                   config_file = system.file("extdata", "config/proteinpaint.toml", package = "ngstk")) {
   config_meta <- eval.config(value = "meta", config = "mut2pp", file = config_file)
   defined_cols <- config_meta[["defined_cols"]][["colnames"]]
   config_format <- eval.config(value = "format", config = "mut2pp", file = config_file)
   config_input <- config_format[[input_type]]
   result <- NULL
   for(i in 1:length(defined_cols)) {
-    col_use <- defined_cols[i]
-    valid_cols <- get_config_value(config_input, col_use, "alias")
-    input_data_col_lower <- tolower(colnames(input_data))
-    col_use_index <- which(input_data_col_lower %in% valid_cols)[1]
-    result <- cbind(result, input_data[[col_use_index]])
-    colnames(result)[i] <- col_use
-    # Process Na value
-    na.replace <- get_config_value(config_input, col_use, "na_replace")
-    if (!is.null(na.replace)) {
-      index <- is.na(result[,i])
-      result[index,i] <- na.replace
-    }
-    # Process value map
-    raw_value <- get_config_value(config_input, col_use, "raw")
-    new_value <- get_config_value(config_input, col_use, "new")
-    if (!is.null(raw_value)) {
-      for(x in 1:length(raw_value)) {
-        result[,i] <- str_replace(result[,i], raw_value[x], new_value[x])
-      }
-    }
-    # Process extract
-    extract_pattern <- get_config_value(config_input, col_use, "extract_pattern")
-    if (!is.null(extract_pattern)) {
-      result[,i] <- str_extract(result[,i], extract_pattern)
-    }
+    result <- handler_proteinpaint(result, config_input, defined_cols, 
+                                   input_data, i)
   }
   return(result)
 }
+
+#' Function to convert fusion data to ProteinPaint \url{https://pecan.stjude.org/pp} input format.
+#'
+#' @param input_data A gene fusions data.frame need to be converted to ProteinPaint input.
+#' @param input_type Point the input data format (fusioncatcher or others)
+#' @param config_file ngstk ProteinPaint configuration file path, default is 
+#' system.file("extdata", "config/proteinpaint.toml", package = "ngstk")
+#' 
+#' @return
+#' A data frame
+#' @export
+#' @examples
+#' demo_file <- system.file("extdata", "demo/fusion2pp_fusioncatcher.txt", package = "ngstk")
+#' input_data <- read.table(demo_file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+#' disease <- "B-ALL"
+#' sampletype <- "diagnose"
+#' input_data <- data.frame(input_data, disease, sampletype)
+#' input_data$disease <- as.character(input_data$disease)
+#' result <- fusion2pp(input_data, input_type = "fusioncatcher")
+fusion2pp <- function(input_data, input_type = "fusioncatcher", 
+                      config_file = system.file("extdata", "config/proteinpaint.toml", 
+                                                package = "ngstk")) {
+  config_meta <- eval.config(value = "meta", config = "fusion2pp", file = config_file)
+  defined_cols <- config_meta[["defined_cols"]][["colnames"]]
+  config_format <- eval.config(value = "format", config = "fusion2pp", file = config_file)
+  config_input <- config_format[[input_type]]
+  result <- NULL
+  for(i in 1:length(defined_cols)) {
+    result <- handler_proteinpaint(result, config_input, defined_cols, 
+                                   input_data, i)
+  }
+  return(result)
+}
+
+#' Function to convert fusion data to ProteinPaint heatmap meta rows \url{https://pecan.stjude.org/pp} input format.
+#'
+#' @param input_data A gene fusions data.frame need to be converted to ProteinPaint input.
+#' @param input_type Point the input data format (fusioncatcher or others)
+#' @param config_file ngstk ProteinPaint configuration file path, default is 
+#' 
+#' @return
+#' A data frame
+#' @export
+#' @examples
+#' demo_file <- system.file("extdata", "demo/fusion2pp_fusioncatcher.txt", package = "ngstk")
+#' input_data <- read.table(demo_file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+#' disease <- "B-ALL"
+#' sampletype <- "diagnose"
+#' input_data <- data.frame(input_data, disease, sampletype)
+#' input_data$disease <- as.character(input_data$disease)
+#' result <- fusion2pp_meta(input_data, input_type = "fusioncatcher")
+fusion2pp_meta <- function(input_data, input_type = "fusioncatcher", 
+                           config_file = system.file("extdata", "config/proteinpaint.toml", 
+                                                     package = "ngstk")) {
+  config_meta <- eval.config(value = "meta", config = "fusion2pp_meta", file = config_file)
+  defined_cols <- config_meta[["defined_cols"]][["colnames"]]
+  config_format <- eval.config(value = "format", config = "fusion2pp_meta", file = config_file)
+  config_input <- config_format[[input_type]]
+  result <- NULL
+  for(i in 1:length(defined_cols)) {
+    result <- handler_proteinpaint(result, config_input, defined_cols, 
+                                   input_data, i)
+  }
+  return(result)
+}
+
+### Framework utils function ###
 
 get_config_value <- function(config_input, level_1, level_2) {
   config_input[[level_1]][[level_2]]
 }
 
-
-
-#' Function to convert fusion data to ProteinPaint \url{https://pecan.stjude.org/pp} input format.
-#'
-#' @param input_data A gene fusions data.frame need to be converted to ProteinPaint input.
-#' @param format Point the input data format (annovar or others)
-#' @param order Input data cols order (chr, start, end, trascript and aachange)
-#' 
-#' @return
-#' A data frame
-#' @export
-#' @examples
-#' example <- NULL
-fusion2pp <- function(input_data, format = "fusioncatcher", 
-                                    order = list(gene5 = 1, gene3 = 2)
-) {
-  if (format == "fusioncatcher") {
-    
+# na.replace
+handler_na_replace <- function(result, config_input, col_use, last.col) {
+  na.replace <- get_config_value(config_input, col_use, "na_replace")
+  if (!is.null(na.replace)) {
+    index <- is.na(result[,last.col])
+    if (any(index)) {
+      result[index,last.col] <- na.replace
+    }
   }
+  return(result)
 }
 
-#' Function to convert fusion data to ProteinPaint heatmap meta rows \url{https://pecan.stjude.org/pp} input format.
-#'
-#' @param input_data A gene fusions data.frame need to be converted to ProteinPaint input (heatmap meta rows).
-#' @param format Point the input data format (annovar or others)
-#' @param order Input data cols order (chr, start, end, trascript and aachange)
-#' 
-#' @return
-#' A data frame
-#' @export
-#' @examples
-#' example <- NULL
-fusion2pp_meta <- function(input_data, format = "fusioncatcher", 
-                      order = list(sample = 1, gene5 = 2, gene3 = 3)) {
-  if (format == "fusioncatcher") {
-    
+# map old value to new value
+handler_value_map <- function(result, config_input, col_use, last.col) {
+  raw_value <- get_config_value(config_input, col_use, "raw")
+  new_value <- get_config_value(config_input, col_use, "new")
+  if (!is.null(raw_value)) {
+    for(x in 1:length(raw_value)) {
+      result[,last.col] <- str_replace(result[,last.col], raw_value[x], new_value[x])
+    }
   }
+  return(result)
+}
+
+# use str_extract get value
+handler_extract <- function(result, config_input, col_use, last.col) {
+  # Process extract
+  extract_pattern <- get_config_value(config_input, col_use, "extract_pattern")
+  if (!is.null(extract_pattern)) {
+    result[,last.col] <- str_extract(result[,last.col], extract_pattern)
+  }
+  return(result)
+}
+
+
+# use str_split get value
+handler_split <- function(result, config_input, col_use, last.col) {
+  split_flag <- get_config_value(config_input, col_use, "split_flag")
+  split_index <- get_config_value(config_input, col_use, "split_index")
+  split_index <- as.numeric(split_index)
+  if (!is.null(split_flag)) {
+    result[,last.col] <- sapply(str_split(result[,last.col], split_flag), 
+                                function(x) return(x[split_index]))
+  }
+  return(result)
+}
+
+# add prefix
+handler_prefix <- function(result, config_input, col_use, last.col) {
+  prefix_flag <- get_config_value(config_input, col_use, "prefix_flag")
+  if (!is.null(prefix_flag)) {
+    result[,last.col] <- unname(sapply(result[,last.col], 
+                                function(x) {
+                                  if (!str_detect(x, paste0("^", prefix_flag))){
+                                    return(paste0(prefix_flag, x))
+                                  } else {
+                                    return(x)
+                                  }
+                                }))
+  }
+  return(result)
+}
+
+# add postfix
+handler_postfix <- function(result, config_input, col_use, last.col) {
+  postfix_flag <- get_config_value(config_input, col_use, "postfix_flag")
+  if (!is.null(postfix_flag)) {
+    result[,last.col] <- unname(sapply(result[,last.col], 
+                                       function(x) {
+                                         if (!str_detect(x, paste0(postfix_flag, "$"))){
+                                           return(paste0(postfix_flag, x))
+                                         } else {
+                                           return(x)
+                                         }
+                                       }))
+  }
+  return(result)
+}
+
+# lower
+handler_lower <- function(result, config_input, col_use, last.col) {
+  flag <- get_config_value(config_input, col_use, "lower")
+  if (!is.null(flag)) {
+    result[,last.col] <- tolower(result[,last.col])
+  }
+  return(result)
+}
+
+# upper
+handler_upper <- function(result, config_input, col_use, last.col) {
+  flag <- get_config_value(config_input, col_use, "upper")
+  if (!is.null(flag)) {
+    result[,last.col] <- tolower(result[,last.col])
+  }
+  return(result)
+}
+
+# main interface for proteinpaint data process
+handler_proteinpaint <- function(result, config_input, defined_cols, input_data, last.col) {
+  col_use <- defined_cols[last.col]
+  valid_cols <- get_config_value(config_input, col_use, "alias")
+  valid_cols <- tolower(valid_cols)
+  input_data_col_lower <- tolower(colnames(input_data))
+  col_use_index <- which(input_data_col_lower %in% valid_cols)[1]
+  old_op <- options()
+  options(stringsAsFactors = F)
+  if (is.na(col_use_index)) {
+    empty.col <- rep("NA", nrow(result)) 
+    result <- cbind(result, empty.col)
+    warning(sprintf("%s were not exists or not be recognize correctly in input data!", col_use))
+  } else {
+    result <- cbind(result, input_data[[col_use_index]])
+  }
+  result <- as.data.frame(result)
+  colnames(result)[last.col] <- col_use
+  result <- handler_na_replace(result, config_input, col_use, last.col)
+  result <- handler_lower(result, config_input, col_use, last.col)
+  result <- handler_upper(result, config_input, col_use, last.col)
+  result <- handler_value_map(result, config_input, col_use, last.col)
+  result <- handler_extract(result, config_input, col_use, last.col)
+  result <- handler_split(result, config_input, col_use, last.col)
+  result <- handler_prefix(result, config_input, col_use, last.col)
+  result <- handler_postfix(result, config_input, col_use, last.col)
+  options(old_op)
+  return(result)
 }
--- tests/testthat/test_b_proteinpaint.R
+++ tests/testthat/test_b_proteinpaint.R
@@ -13,3 +13,37 @@ test_that("mut2pp", {
   expect_that(result_colnames[6], equals("class"))
   expect_that(result_colnames[8], equals("sample"))
 })
+
+
+test_that("fusion2pp", {
+  demo_file <- system.file("extdata", "demo/fusion2pp_fusioncatcher.txt", package = "ngstk")
+  input_data <- read.table(demo_file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+  disease <- "B-ALL"
+  sampletype <- "diagnose"
+  input_data <- data.frame(input_data, disease, sampletype)
+  input_data$disease <- as.character(input_data$disease)
+  result <- fusion2pp(input_data, input_type = "fusioncatcher")
+  result_colnames <- colnames(result)
+  colnames_len <- length(result_colnames)
+  expect_that(colnames_len, equals(13))
+  expect_that(result_colnames[1], equals("disease"))
+  expect_that(result_colnames[3], equals("gene_a"))
+  expect_that(result_colnames[6], equals("position_a"))
+  expect_that(result_colnames[13], equals("patient"))
+  expect_that(result[1,3], equals("TCF3"))
+  expect_that(result[1,7], equals("PBX1"))
+  expect_that(result[7,5], equals("chr4"))
+  expect_that(result[7,6], equals("144801564"))
+  expect_that(result[7,12], equals("-"))
+})
+
+
+test_that("fusion2pp_meta", {
+  demo_file <- system.file("extdata", "demo/fusion2pp_fusioncatcher.txt", package = "ngstk")
+  input_data <- read.table(demo_file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+  disease <- "B-ALL"
+  sampletype <- "diagnose"
+  input_data <- data.frame(input_data, disease, sampletype)
+  input_data$disease <- as.character(input_data$disease)
+  result <- fusion2pp_meta(input_data, input_type = "fusioncatcher")
+})
