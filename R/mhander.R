f5c3791e747a23525d62d41e89fbea3fb2f9452d
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2017-09-30 11:11:07 +0800

    mhander flag be added

--- R/mhander.R
+++ R/mhander.R
@@ -4,7 +4,8 @@ mhandler <- function(hander_data, config_input, mhander_funs = NULL, extra_param
   options(stringsAsFactors = F)
   if (!is.null(mhander_funs)) {
     for (i in mhander_funs) {
-      hander_data <- do.call(i, list(hander_data = hander_data, config_input = config_input, extra_params = extra_params))
+      hander_data <- do.call(i, list(hander_data = hander_data, 
+                                     config_input = config_input, extra_params = extra_params))
     }
     
   }
@@ -13,10 +14,13 @@ mhandler <- function(hander_data, config_input, mhander_funs = NULL, extra_param
 }
 
 # two colum match
-mhander_fusions_left_match <- function(hander_data, config_input, extra_params = list(gene_5 = 1, left_gene = NULL)) {
+mhander_fusions_left_match <- function(hander_data, config_input, 
+                                       extra_params = list(gene_5 = 1, left_gene = NULL, 
+                                                           left_match = TRUE)) {
   left_gene <- extra_params$left_gene
   gene_5 <- extra_params$gene_5
-  if (!is.null(extra_params$left_gene)) {
+  left_match <- extra_params$left_match
+  if (!is.null(left_gene) && !is.null(left_match) && left_match) {
     index <- str_detect(hander_data[, gene_5], sprintf("^%s$", left_gene))
     return(hander_data[index, ])
   } else {
@@ -24,10 +28,13 @@ mhander_fusions_left_match <- function(hander_data, config_input, extra_params =
   }
 }
 
-mhander_fusions_right_match <- function(hander_data, config_input, extra_params = list(gene_3 = 2, right_gene = NULL)) {
+mhander_fusions_right_match <- function(hander_data, config_input, 
+                                        extra_params = list(gene_3 = 2, right_gene = NULL, 
+                                                            right_match = TRUE)) {
   right_gene <- extra_params$right_gene
   gene_3 <- extra_params$gene_3
-  if (!is.null(extra_params$right_gene)) {
+  right_match <- extra_params$right_match
+  if (!is.null(right_gene) && !is.null(right_match) && right_match ) {
     index <- str_detect(hander_data[, gene_3], sprintf("^%s$", right_gene))
     return(hander_data[index, ])
   } else {
@@ -35,12 +42,14 @@ mhander_fusions_right_match <- function(hander_data, config_input, extra_params 
   }
 }
 
-mhander_fusions_any_match <- function(hander_data, config_input, extra_params = list(gene_5 = 1, gene_3 = 2, 
-  any_gene = NULL)) {
+mhander_fusions_any_match <- function(hander_data, config_input, 
+                                      extra_params = list(gene_5 = 1, gene_3 = 2, 
+  any_gene = NULL, any_match = TRUE)) {
   any_gene <- extra_params$any_gene
   gene_5 <- extra_params$gene_5
   gene_3 <- extra_params$gene_3
-  if (!is.null(any_gene)) {
+  any_match <- extra_params$any_match
+  if (!is.null(any_gene) && !is.null(any_match) && any_match ) {
     index_1 <- str_detect(hander_data[, gene_5], sprintf("^%s$", any_gene))
     index_2 <- str_detect(hander_data[, gene_3], sprintf("^%s$", any_gene))
     return(hander_data[index_1 | index_2, ])
@@ -50,15 +59,37 @@ mhander_fusions_any_match <- function(hander_data, config_input, extra_params = 
 }
 
 mhander_fusions_full_match <- function(hander_data, config_input, extra_params = list(gene_5 = 1, gene_3 = 2, 
-  left_gene = NULL, right_gene = NULL)) {
+  left_gene = NULL, right_gene = NULL, full_match = TRUE)) {
+  left_gene <- extra_params$left_gene
+  right_gene <- extra_params$right_gene
+  full_match <- extra_params$full_match
+  gene_5 <- extra_params$gene_5
+  gene_3 <- extra_params$gene_3
+  if (!is.null(left_gene) && !is.null(right_gene) && !is.null(full_match) && full_match) {
+    index_1 <- str_detect(hander_data[, gene_5], sprintf("^%s$", left_gene))
+    index_2 <- str_detect(hander_data[, gene_3], sprintf("^%s$", right_gene))
+    return(hander_data[index_1 & index_2, ])
+  } else {
+    return(hander_data)
+  }
+}
+
+mhander_fusions_anyfull_match <- function(hander_data, config_input, 
+                                          extra_params = list(gene_5 = 1, gene_3 = 2, 
+                                          left_gene = NULL, right_gene = NULL,
+                                          anyfull_match = TRUE)) {
   left_gene <- extra_params$left_gene
   right_gene <- extra_params$right_gene
   gene_5 <- extra_params$gene_5
   gene_3 <- extra_params$gene_3
-  if (!is.null(left_gene) & !is.null(right_gene)) {
+  anyfull_match <- extra_params$anyfull_match
+  if (!is.null(left_gene) && !is.null(right_gene) && !is.null(anyfull_match) && anyfull_match) {
     index_1 <- str_detect(hander_data[, gene_5], sprintf("^%s$", left_gene))
     index_2 <- str_detect(hander_data[, gene_3], sprintf("^%s$", right_gene))
-    return(hander_data[index_1 & index_2, ])
+    
+    index_3 <- str_detect(hander_data[, gene_5], sprintf("^%s$", right_gene))
+    index_4 <- str_detect(hander_data[, gene_3], sprintf("^%s$", left_gene))
+    return(hander_data[(index_1 & index_2) | (index_3 & index_4), ])
   } else {
     return(hander_data)
   }
--- tests/testthat/test_d_filter.R
+++ tests/testthat/test_d_filter.R
@@ -2,12 +2,26 @@
 test_that("fusions_filter", {
   demo_file <- system.file("extdata", "demo/proteinpaint/fusion2pp_fusioncatcher.txt", package = "ngstk")
   input_data <- read.table(demo_file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
-  result_1 <- fusions_filter(input_data, mhander_extra_params = list(gene_5 = 1, gene_3 = 2, any_gene = "TCF3"))
-  result_2 <- fusions_filter(input_data, mhander_extra_params = list(gene_3 = 2, right_gene = "GYPA"))
-  result_3 <- fusions_filter(input_data, mhander_extra_params = list(gene_5 = 1, left_gene = "GYPA"))
-  result_3 <- fusions_filter(input_data, mhander_extra_params = list(gene_5 = 1, left_gene = "GYPA"))
-  result_4 <- fusions_filter(input_data, mhander_extra_params = list(gene_5 = 1, gene_3 = 2, left_gene = "GYPE", 
-    right_gene = "GYPA"))
+  
+  mhander_extra_params = list(gene_5 = 1, gene_3 = 2, any_gene = "TCF3", any_match = TRUE)
+  result_1 <- fusions_filter(input_data, mhander_extra_params = mhander_extra_params)
+  
+  mhander_extra_params = list(gene_3 = 2, right_gene = "GYPA", right_match = TRUE)
+  result_2 <- fusions_filter(input_data, mhander_extra_params = mhander_extra_params)
+  
+  mhander_extra_params = list(gene_5 = 1, left_gene = "GYPA", left_match = TRUE)
+  result_3 <- fusions_filter(input_data, mhander_extra_params = mhander_extra_params)
+  
+  mhander_extra_params = list(gene_5 = 1, gene_3 = 2, 
+                              left_gene = "GYPE", right_gene = "GYPA", 
+                              full_match = TRUE)
+  result_4 <- fusions_filter(input_data, mhander_extra_params = mhander_extra_params)
+  
+  mhander_extra_params = list(gene_5 = 1, gene_3 = 2, 
+                              left_gene = "GYPE", right_gene = "GYPA", 
+                              anyfull_match = TRUE)
+  result_5 <- fusions_filter(input_data, mhander_extra_params = mhander_extra_params)
+  
   expect_that(nrow(result_1), equals(5))
   expect_that(nrow(result_2), equals(2))
   expect_that(nrow(result_3), equals(0))
